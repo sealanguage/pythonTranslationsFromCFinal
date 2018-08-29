d4a6230d0927d09352060705dd5ee077babdccbd
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-08-29 00:32:26 +0000

    final working code

--- mario.py
+++ mario.py
@@ -1,34 +1,26 @@
 from cs50 import get_int
 
 height = get_int("Height: ")
-heightN = height
-rowP = height
-space = rowP
 
-while (height < 1 or height > 23):
+while (height < 0 or height > 23):
     print("Usage: height between 1 and 23")
-    break
+    height = get_int("Height: ")
 
-while (height > 1 and height < 23):
-    print("value of height = ", height)
+rowP = height
 
-# for every row in height
-# print spaces height - 1
-# print hashes remainder of spaces - height
-# print a new line
+while (height >= 1 and height < 24):
+
+    # for every row in height
+    # print spaces height - 1
+    # print hashes remainder of spaces - height
+    # print a new line
 
     # for height number of rows
     for rowP in range(1, (height + 1), 1):
         rowP = rowP
-        print("^ " * (height - rowP), end = "")
+        print(" " * (height - rowP), end="")
 
         # for space in range(rowP, 1, -1):
-        print("# " * (rowP)),
+        print("#" * (rowP), end="#\n"),
 
     break
-
-
-
-
-
-
