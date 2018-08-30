d6ea22e75587dfbc03ef2ab41002e7f5e2a9543b
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-08-30 23:44:33 +0000

    change modulo code working, get and validate dollars not working

--- cash.py
+++ cash.py
@@ -2,16 +2,35 @@ from cs50 import get_float
 
 dollars = get_float("Dollars: ")
 coins = 0
+changeOwed = round(dollars)
+# print("ROUND changeOwed: ", changeOwed)
+
+# if(dollars >= 1):
+#     changeOwed = round(dollars * 100)
+#     print(changeOwed)
+#     # print("ROUND changeOwed: ", changeOwed)
+
+# #     dollars = get_float("Dollars: ")
+# #     changeOwed = round(dollars * 100)
+# #     print("THIS IS INPUT ", dollars)
+
+# elif (dollars > 0 or dollars < 1):
+#     changeOwed = int(dollars * 100)
+#     round(changeOwed, 0)
+#     # print("changeOwed < 1: ", changeOwed)
+
+# elif (dollars <= 0):
+#     dollars = get_float("Dollars: ")
 
 
-while(dollars <= 0):
-    dollars = get_float("Dollars: ")
-    print(dollars)
-    break
 
 # change dollars to cents
 changeOwed = round(dollars * 100)
 print(changeOwed)
+print("ROUND changeOwed: ", changeOwed)
+
+# change dollars to cents
+# changeOwed = round(dollars)
 
 while (changeOwed > 25):
     dollars = dollars % 25
