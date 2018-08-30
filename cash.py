26927eafe0ca7cfb6a7e5400f14dd6c8b7beb145
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-08-30 22:22:38 +0000

    change modulo code working, get and validate dollars not working

--- cash.py
+++ cash.py
@@ -17,41 +17,25 @@ while (changeOwed > 25):
     dollars = dollars % 25
     coins += 1
     changeOwed = changeOwed - 25
-    print("change owed = ", changeOwed)
-    print("coins = ", coins)
+    # print(changeOwed)
+    print(coins)
 while (changeOwed > 10):
     dollars = dollars % 10
     coins += 1
     changeOwed = changeOwed - 10
-    print("change owed = ", changeOwed)
-    print("coins = ", coins)
+    print(changeOwed)
+    print(coins)
 while (changeOwed > 5):
     dollars = dollars % 5
     coins += 1
     changeOwed = changeOwed - 5
-    print("change owed = ", changeOwed)
-    print("coins = ", coins)
+    print(changeOwed)
+    print(coins)
 while (changeOwed > 1):
     dollars = dollars % 1
     coins += 1
     changeOwed = changeOwed - 1
-    print("change owed = ", changeOwed)
-    print("coins = ", coins)
-
-
-
-
-# # prints Hello Geek 3 Times
-# count = 0
-# while (count < 3):
-#     count = count+1
-#     print("Hello Geek")
-
-
-
-# i = 1
-# while i < 6:
-#   print(i)
-#   if i == 3:
-#     break
-#   i += 1
+    print(changeOwed)
+    print(coins)
+    print()
+    break
